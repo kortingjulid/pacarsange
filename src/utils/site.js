8ca397fbb5ac2e641ce7cf726f4ababd77148b66
kortingjulid <kortingjulid@gmail.com>
2025-07-17 20:17:54 +0700

    Update site.js

--- src/utils/site.js
+++ src/utils/site.js
@@ -4,6 +4,6 @@ export const url = 'https://pacarsange.pages.dev';
 export const terbit = '2024-12-17T00:01:03Z';
 
 export const google = 'dzNqvBNufkxRDPtXsRmv3sEKR8ICiEe0cJAZFTxYDmE';
-export const yandex = 'e50ba6e1a25786e6'
+export const yandex = 'c8b4f4afd13b505d'
 
 export const index = 'f91d1ca8-5d41-423c-94e9-13d1974848c5';
